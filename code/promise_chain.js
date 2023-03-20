3bfb60bb5026a7c74f9e62039457c305688908a1
choi4624 <choi4624@gmail.com>
2023-03-20 10:11:56 +0900

    chain_promise & array promise

--- code/promise_chain.js
+++ code/promise_chain.js
@@ -11,9 +11,14 @@ const promise = new Promise((resolve, reject) => {
     }
 }).then((message) => {
         console.log(message);
-        return new Promise((resolve,reject) => {
-            resolve('두번째 체인');
-        })
+        
+        
+        // return new Promise((resolve,reject) => {
+        //     resolve('두번째 체인');
+        // })
+
+        // 바로 두번째 체인 처리하는 방법 
+        return Promise.resolve("두번째 체인");
     })
     .then((message) => {
         console.log('2',message);
@@ -26,3 +31,15 @@ const promise = new Promise((resolve, reject) => {
 
 // new promise로 then catch finally 한번에 promise로 넣음 
 // 한문장 코딩 
+
+const promise1 = Promise.resolve('성공 1');
+const promise2 = Promise.reject('성공 2');
+
+// 배열 형식으로 출력이 나옴 
+Promise.allSettled([promise1,promise2])
+        .then((result) =>{
+            console.log(result);
+        }) // 한개의 promise라도 error가 나오면 error한 promise만 나옴 Allsettled 하면 결과가 개별적으로 나옴 
+        .catch((error) => {
+            console.error(error);
+        })
