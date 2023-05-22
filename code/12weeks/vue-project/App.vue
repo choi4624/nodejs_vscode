b9bb56da9ba56eec6281f105b44a6bc4e3066ccb
choi4624 <choi4624@gmail.com>
2023-05-22 09:52:53 +0900

    기본 세팅

---
+++ code/12weeks/vue-project/App.vue
@@ -0,0 +1,12 @@
+<!-- setup 안붙이면 다른 api를 불러옴 -->
+<script setup>
+
+</script>
+
+<style>
+
+</style>
+
+<template>
+
+</template>
---
+++ code/12weeks/vue-project/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// ./App.vue 의 컴포넌트 파일을 넣기 
+
+createApp(App).mount('#app')
